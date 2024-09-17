753abca1c53ceab2838f12f85017cc89b19cb562
RazEbrat <raz.tekschool@gmail.com>
2024-09-17 16:44:49 -0400

    initial commit

--- src/demo/DemoTest.java
+++ src/demo/DemoTest.java
@@ -5,5 +5,11 @@ public class DemoTest {
 	public static void main(String[] args) {
 		System.out.println("Raz");
 	}
+	
+	public static int addNums (int a, int b) {
+		int result = a +b;
+		
+		return result;
+	}
 
 }
